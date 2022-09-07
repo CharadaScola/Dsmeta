a1bd6e8120f9e27790359391a311fc54c6fdbf13
Matheus Scola <matheus.fernan01@gmail.com>
2022-09-07 11:54:35 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import NotificationnButton from "./componentes/NotificationButton"
+
+
 
 function App() {
   return(
+      <>
       <h1>Olá mundo!</h1>
+      <NotificationnButton />
+      <NotificationnButton />
+      </>
   )
 }
 
